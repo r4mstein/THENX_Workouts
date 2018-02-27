f196f1ea768978a2b1dd936b99a92f5ce6e26386
r4mstein <r4mstein@ukr.net>
2018-02-27 11:34:25 +0200

    Initial commit

---
+++ app/src/main/java/r4mstein/ua/thenxworkouts/root/App.java
@@ -0,0 +1,15 @@
+package r4mstein.ua.thenxworkouts.root;
+
+import android.app.Application;
+
+/**
+ * Created by Alex Shtain on 27.02.2018.
+ */
+
+public final class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
